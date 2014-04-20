c15d6af8f029c895fa30c3cd2c8f45f1f7a561ae
Nick Johnson <nick@Nicks-MacBook-Pro.local>
2014-04-20 00:04:38 -0500

    Initial Commit

---
+++ WNJ_Program6/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  WNJ_Program6
+//
+//  Created by Nick Johnson on 4/20/14.
+//  Copyright (c) 2014 UAH. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
